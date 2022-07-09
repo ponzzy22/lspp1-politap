355bc07c5126262e942d33433f565e32b5578d84
Ponsianus Jopi <90673400+ponzzy22@users.noreply.github.com>
2022-07-10 01:45:21 +0700

    v1

---
+++ resources/views/client/tutorial.blade.php
@@ -0,0 +1,80 @@
+@extends('layout.client')
+@section('judul')
+    Prosedur Sertifikasi | LSP POLITAP
+@endsection
+
+@section('layanan')
+    active
+@endsection
+
+@section('isi')
+    <!-- ***** Header ***** -->
+    <div style="background-image: url('{{ asset('general/assets/images/head1.jpg') }}')" class="page-heading header-text">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1><i class="fas fa-pencil-square"></i> Prosedur Pendaftaran Sertifikasi </h1>
+                    <span>Lembaga Sertifikasi Profesi Politeknik Negeri Ketapang</span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- ***** Akhir Header ***** -->
+
+    <!-- ***** Konten ***** -->
+    <!-- Gambar -->
+    <div id="logo" class="more-info about-info">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="more-info-content">
+                        <div class="col-md-8 align-self-center">
+                        </div>
+                        <div class="col-md-12">
+                            @foreach ($tutorial2 as $asu)
+                                @if ($asu->image)
+                                    <img src="{{ $asu->image }}" width="100%">
+                                @else
+                                <img src="{{ asset('general/assets/images/no-image-min.jpg') }}" width="100%">
+                                @endif
+                            @endforeach
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div><br>
+
+    <!-- Video -->
+    <div id="logo" class="more-info about-info">
+        <div class="container">
+
+            <div class="section-heading">
+                <h2>Video Tutorial <em>Pendaftaran Sertifikasi </em></h2>
+                <span>▬▬▬▬▬<em>▬▬▬▬▬</em></span>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="more-info-content">
+                        <div class="col-md-8 align-self-center">
+                        </div>
+                        <div class="col-md-12">
+                            @foreach ($tutorial as $asu)
+                                @if ($asu->image)
+                                    <video width="100%" controls>
+                                        <source src="{{ $asu->image }}" type="video/mp4">
+                                        Your browser does not support HTML video.
+                                    </video>
+                                @else
+                                    <h1 class="text-danger">TIDAK ADA VIDEO</h1>
+                                @endif
+                            @endforeach
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <br><br><br>
+    <!-- ***** Akhir Konten ***** -->
+@endsection
